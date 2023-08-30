b4f7d63cb1f05cba5b4b358aae1359652e6dbe6a
Santiago Donoso <santiagodonoso@gmail.com>
2023-08-30 13:27:06 +0200

    ...

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 
-<footer>
+<footer class="w-full h-4 bg-sky-600 fixed bottom-0">
   Copyright &copy; <?= date('Y') ?>
 </footer>
 
--- header.php
+++ header.php
@@ -13,12 +13,13 @@ require_once __DIR__ . '/_.php';
     <?= "Company : : $the_title" ?? 'My APP' ?>
   </title>
 </head>
-<body>
+<body class="w-full h-full bg-gray-600 text-white">
 
-  <nav>
+  <nav class="flex w-full h-8 bg-sky-600 items-center gap-4 px-4">
     <a href="/">Home</a>
     <a href="/items">Items</a>
     <a href="/users">Users</a>
+    <a href="/login" class="ml-auto">Login</a>
   </nav>
 
 
