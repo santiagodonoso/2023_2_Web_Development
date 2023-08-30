9be8a140d6fb93eabbefdff2224ee49af0876c6c
Santiago Donoso <santiagodonoso@gmail.com>
2023-08-30 11:16:08 +0200

    ...

---
+++ header.php
@@ -0,0 +1,17 @@
+<?php
+require_once __DIR__ . '/_.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>APP</title>
+</head>
+<body>
+
+  <nav>
+    <a href="/">Home</a>
+    <a href="/items">Items</a>
+  </nav>
--- index.php
+++ index.php
@@ -1,20 +1,12 @@
-<?php
-require_once __DIR__ . '/_.php';
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>APP</title>
-</head>
-<body>
+
+
   <form>
     <label for="">
       Item name min <?= ITEM_NAME_MIN_LEN ?> max <?= ITEM_NAME_MAX_LEN ?>
     </label>
     <input type="text">
   </form>
-</body>
-</html>
+
+
+
