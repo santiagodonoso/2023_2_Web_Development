403ccc227cf29c60e6b194fdf6d1ce53a24792e4
Santiago Donoso <santiagodonoso@gmail.com>
2023-08-30 11:18:31 +0200

    ...

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
+<?php
+require_once __DIR__ . '/header.php';
+?>
 
+<form>
+  <label for="">
+    Item name min <?= ITEM_NAME_MIN_LEN ?> max <?= ITEM_NAME_MAX_LEN ?>
+  </label>
+  <input type="text">
+</form>
 
-
-  <form>
-    <label for="">
-      Item name min <?= ITEM_NAME_MIN_LEN ?> max <?= ITEM_NAME_MAX_LEN ?>
-    </label>
-    <input type="text">
-  </form>
-
-
+<?php
+require_once __DIR__ . '/footer.php';
+?>
 
