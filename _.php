cce7f44dbff994830aa3ff8866aa7df9551032d9
Santiago Donoso <santiagodonoso@gmail.com>
2023-08-30 09:04:12 +0200

    ...

--- _.php
+++ _.php
@@ -9,11 +9,18 @@ function _validate_item_name(){
     exit();
   }
   $_POST['item_name'] = trim($_POST['item_name']);
+
   if( strlen($_POST['item_name']) < ITEM_NAME_MIN_LEN){
     throw new Exception('item_name min '.ITEM_NAME_MIN_LEN.' characters');
     exit();    
   }
+
+  if( strlen($_POST['item_name']) < ITEM_NAME_MIN_LEN){
+    throw new Exception('item_name min '.ITEM_NAME_MIN_LEN.' characters');
+    exit();    
+  }  
 }
+
 
 
 ?>
