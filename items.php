acc6817d568e4e35e249888fcea28868e5407c79
Santiago Donoso <santiagodonoso@gmail.com>
2023-08-30 11:20:59 +0200

    ...

--- items.php
+++ items.php
@@ -1,11 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  
-</body>
-</html>
+<?php
+require_once __DIR__ . '/header.php';
+?>
+
+
+<h1>
+  Items
+</h1>
+
+
+
+
+<?php
+require_once __DIR__ . '/footer.php';
+?>
