1e3a57f972a9d5c38074acb4fc923ed142451b42
Santiago Donoso <santiagodonoso@gmail.com>
2023-08-30 08:54:08 +0200

    ...

--- _.php
+++ _.php
@@ -1,39 +1,18 @@
 <?php
 
-function _validate_user_name(){
-  if( ! isset($_POST['name']) ){
-    throw new Exception('name missing');
+
+define('ITEM_NAME_MIN_LEN', 2);
+
+function _validate_item_name(){
+  if(!isset($_POST['item_name'])){
+    throw new Exception('item_name missing');
+    exit();
   }
-
-  $_POST['name'] = trim($_POST['name']);
-
-  if( strlen($_POST['name']) < 2 ){
-    throw new Exception('name min 2');
+  $_POST['item_name'] = trim($_POST['item_name']);
+  if( strlen($_POST['item_name']) < ITEM_NAME_MIN_LEN){
+    throw new Exception('item_name min 2 characters');
+    exit();    
   }
-  if( strlen($_POST['name']) > 20 ){
-    throw new Exception('name max 20');
-  }  
-  // Success
-  return true;
-}
-
-
-function _validate_last_name(){
-  if( ! isset($_POST['last_name']) ){
-    throw new Exception('last_name missing');
-  }
-
-  $_POST['last_name'] = trim($_POST['last_name']);
-
-
-  if( strlen($_POST['last_name']) < 2 ){
-    throw new Exception('last_name min 2');
-  }
-  if( strlen($_POST['last_name']) > 20 ){
-    throw new Exception('last_name max 20');
-  }  
-  // Success
-  return true;  
 }
 
 
