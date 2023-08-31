0de54bf00a32d76d1d2e741fb8a94401e8178d17
Santiago Donoso <santiagodonoso@gmail.com>
2023-08-31 13:00:53 +0200

    ...

---
+++ api/items.php
@@ -0,0 +1,8 @@
+<?php
+  // items?color=blue
+  // URL friendly, search engine friendly, SEO
+  // items/blue
+  //http://127.0.0.1/items?color=blue
+  echo $_GET['color'];
+  echo $_GET['gender'];
+?>
