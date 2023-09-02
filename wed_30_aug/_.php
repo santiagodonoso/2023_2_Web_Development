0e60d5ee0f215767d3e19b87f0700d86448aca06
Santiago Donoso <santiagodonoso@gmail.com>
2023-09-02 19:18:16 +0200

    ...

---
+++ wed_30_aug/_.php
@@ -0,0 +1,27 @@
+<?php
+
+
+define('ITEM_NAME_MIN_LEN', 2);
+define('ITEM_NAME_MAX_LEN', 20);
+
+function _validate_item_name(){
+  if(!isset($_POST['item_name'])){
+    throw new Exception('item_name missing');
+    exit();
+  }
+  $_POST['item_name'] = trim($_POST['item_name']);
+
+  if( strlen($_POST['item_name']) < ITEM_NAME_MIN_LEN){
+    throw new Exception('item_name min '.ITEM_NAME_MIN_LEN.' characters');
+    exit();    
+  }
+
+  if( strlen($_POST['item_name']) > ITEM_NAME_MAX_LEN){
+    throw new Exception('item_name max '.ITEM_NAME_MAX_LEN.' characters');
+    exit();    
+  }  
+}
+
+
+
+?>
---
+++ wed_30_aug/footer.php
@@ -0,0 +1,8 @@
+
+<footer class="w-full h-4 bg-sky-600 fixed bottom-0">
+  Copyright &copy; <?= date('Y') ?>
+</footer>
+
+
+</body>
+</html>
