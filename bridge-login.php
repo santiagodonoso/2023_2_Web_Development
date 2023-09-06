81cea3dcdb8d516f7971a7a71598077d35a3770f
Santiago Donoso <santiagodonoso@gmail.com>
2023-09-06 09:45:21 +0200

    ...

--- bridge-login.php
+++ bridge-login.php
@@ -4,4 +4,14 @@ echo $email;
 $password = $_POST['password'];
 echo $password;
 // header('Location: /admin.php');
+if( $email == 'a' ){ // success
+  session_start();
+  $_SESSION['email'] = $email;
+  header('Location: /admin.php');
+  exit();
+}
+
+header('Location: /login.php');
+exit();
+
 ?>
