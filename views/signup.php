0d9466daf8284a04911d79af92e8a9fb3bfc3843
Santiago Donoso <santiagodonoso@gmail.com>
2023-09-13 09:01:07 +0200

    ...

--- views/signup.php
+++ views/signup.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/header.php';
+require_once __DIR__ . '/comp_header.php';
 ?>
 
 <h1>
@@ -7,5 +7,5 @@ require_once __DIR__ . '/header.php';
 </h1>
 
 <?php
-require_once __DIR__ . '/footer.php';
+require_once __DIR__ . '/comp_footer.php';
 ?>
