fd88dac68a00e173b155f3ed9bf978f505e16064
Santiago Donoso <santiagodonoso@gmail.com>
2023-09-14 12:51:51 +0200

    ...

--- api-delete-user.php
+++ api-delete-user.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 require_once __DIR__ . '/_.php';
 
 try{
@@ -9,7 +9,7 @@ try{
   $db = _db();
   $q = $db->prepare(' DELETE FROM 
                       users WHERE user_id = :user_id');
-  $q->bindValue(':user_id', $_POST['user_id']);
+  $q->bindValue(':user_id', $_SESSION['user_id']);
   $q->execute();
   $deleted_rows = $q->rowCount(); 
   http_response_code(204);
