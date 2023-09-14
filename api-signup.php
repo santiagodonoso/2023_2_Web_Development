d2173b7aaae431e2d137a91e7cebc5734dfcf218
Santiago Donoso <santiagodonoso@gmail.com>
2023-09-14 11:03:08 +0200

    ...

--- api-signup.php
+++ api-signup.php
@@ -8,21 +8,23 @@ try{
 
   $db = _db();
   $q = $db->prepare(' INSERT INTO 
-                      users (id, name) 
-                      VALUES (null,:user_name)'); 
+                      users (user_id, user_name, user_last_name) 
+                      VALUES (null, :user_name, :user_last_name)'); 
 
   // bind and prevent SQL injections
   $q->bindValue(':user_name', $_POST['user_name']);
+  $q->bindValue(':user_last_name', $_POST['user_last_name']);
   // Get the user's id
 
   $q->execute();
   $user_id = $db->lastInsertId();
 
   echo json_encode([
-      'info' => 'user created',
-      'id' => $user_id
+      'user_id' => (int)$user_id,
+      'user_name' => $_POST['user_name'],
+      'user_last_name' => $_POST['user_last_name']
     ]);
-    
+
 }catch(Exception $e){
   http_response_code(400);
   echo json_encode(['info' => 'database error']);
