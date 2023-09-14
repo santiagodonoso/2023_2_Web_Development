2da654daae97c08749f3450ed101bfe443e9a7ee
Santiago Donoso <santiagodonoso@gmail.com>
2023-09-14 10:16:32 +0200

    ...

--- api-signup.php
+++ api-signup.php
@@ -3,10 +3,17 @@
 require_once __DIR__ . '/_.php';
 
 try{
+
+  // TODO: validate the user_name
+
   $db = _db();
   $q = $db->prepare(' INSERT INTO 
                       users (id, name) 
-                      VALUES (null, "Santiago")'); 
+                      VALUES (null, :user_name)'); 
+
+  // bind and prevent SQL injections
+  $q->bindValue(':user_name', $_POST['user_name']);
+
   $q->execute();
   echo json_encode(['info' => 'user created']);
 }catch(Exception $e){
