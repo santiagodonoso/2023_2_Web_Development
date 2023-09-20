0760726d38aa93a6e29b540090e3ff3dcfa5ef97
Santiago Donoso <santiagodonoso@gmail.com>
2023-09-20 13:17:06 +0200

    ...

--- views/signup.php
+++ views/signup.php
@@ -7,7 +7,8 @@ require_once __DIR__.'/_header.php';
   <form onsubmit="validate(signup); return false" 
   class="flex flex-col px-4 text-white 
   lg:w-1/3 mx-auto gap-4 [&_input]:border [&_input]:border-black
-  [&_input]:h-10 [&_input]:rounded-md [&_input]:outline-none">
+  [&_input]:h-10 [&_input]:rounded-md [&_input]:outline-none
+  [&_input]:text-black">
 
     <div class="grid">
       <label for="user_name">
