bd59b563f1cf26ad5f5d95de3509b1a31de5d6ab
Santiago Donoso <santiagodonoso@gmail.com>
2023-09-20 10:46:03 +0200

    ...

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 async function signup(){
   const frm = event.target
   console.log(frm)
-  // const conn = await fetch("/api/api-signup.php")
+  const conn = await fetch("/api/api-signup.php", {
+    method : "POST",
+    body : new FormData(frm)
+  })
+  const data = await conn.text()
+  console.log(data)
 }
