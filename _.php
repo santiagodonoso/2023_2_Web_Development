bb10a5813d369bbd434dc40f23ff4788ce1174a4
Santiago Donoso <santiagodonoso@gmail.com>
2023-09-20 10:31:52 +0200

    ...

--- _.php
+++ _.php
@@ -6,6 +6,11 @@ function _validate_user_name(){
   if(!isset($_POST['user_name'])){ 
     throw new Exception('user_name min '.USER_NAME_MIN.' max '.USER_NAME_MAX); 
   }
+  $_POST['user_name'] = trim($_POST['user_name']);
+
+  if( strlen($_POST['user_name']) < USER_NAME_MIN ){
+    throw new Exception('user_name min '.USER_NAME_MIN.' max '.USER_NAME_MAX);
+  }
 }
 
 
