dd9a7b7b4601b2b8c02ad32da8d58e348942b50b
Santiago Donoso <santiagodonoso@gmail.com>
2023-09-20 12:34:43 +0200

    ...

--- views/_header.php
+++ views/_header.php
@@ -3,9 +3,10 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/app.css">
   <title>Document</title>
 </head>
-<body>
+<body class="w-full h-screen bg-sky-600">
   
   <nav>
     <a href="/">Home</a>
