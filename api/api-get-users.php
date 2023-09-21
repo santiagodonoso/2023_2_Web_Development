3f1d41ec6c59408b32b52f0f599a85d72f64e6fc
Santiago Donoso <santiagodonoso@gmail.com>
2023-09-21 13:11:49 +0200

    ...

---
+++ api/api-get-users.php
@@ -0,0 +1,20 @@
+<?php
+require_once __DIR__.'/../_.php';
+try{
+
+  $db = _db();
+  $q = $db->prepare('SELECT * FROM users');
+  $q->execute();
+  $users = $q->fetchAll();
+  echo json_encode($users);
+
+}catch(Exception $e){
+  try{
+    if( ! $e->getCode() || ! $e->getMessage()){ throw new Exception(); }
+    http_response_code($e->getCode());
+    echo json_encode(['info'=>$e->getMessage()]);
+  }catch(Exception $ex){
+    http_response_code(500);
+    echo json_encode($ex); 
+  }
+}
