bd33a3a114f313223e8bfcae8d2d23cb42e0c138
Santiago Donoso <santiagodonoso@gmail.com>
2023-09-21 09:22:26 +0200

    ...

--- database.sql
+++ database.sql
@@ -6,11 +6,13 @@ CREATE TABLE users(
   user_last_name      TEXT,
   user_email          TEXT,
   user_password       TEXT,
+  user_role           TEXT,
   user_created_at     TEXT,
   user_updated_at     TEXT,
   PRIMARY KEY(user_id)
 ) WITHOUT ROWID;
 
+INSERT INTO users VALUES ("1", "A", "B", "@a", "password", "Admin", "1", "1");
 
 
 
