2db1c800f485a992d2ff3e744250315e8c427b04
Santiago Donoso <santiagodonoso@gmail.com>
2023-09-21 09:25:59 +0200

    ...

--- _.php
+++ _.php
@@ -1,5 +1,25 @@
 <?php
 
+// ##############################
+function _db(){
+	try{
+	  $user_name = 'root';
+	  $user_password = ''; // MAC the password root, password
+	  $db_connection = "mysql:host=localhost; dbname=company; charset=utf8mb4";
+	
+	  // PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+	  //   PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ   [{}]    $user->id
+	  $db_options = array(
+		PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC // [['id'=>1, 'name'=>'A'],[]]  $user['id']
+	  );
+	  return new PDO( $db_connection, $user_name, $user_password, $db_options );
+	}catch( PDOException $e){
+	  throw new Exception('ups... system under maintainance', 500);
+	  exit();
+	}	
+}
+
 
 // ##############################
 define('USER_NAME_MIN', 2);
