c6134683770049f767c0c097bcecdd766525cffc
Santiago Donoso <santiagodonoso@gmail.com>
2023-09-28 12:33:12 +0200

    ...

--- seeders/seed_employees.php
+++ seeders/seed_employees.php
@@ -8,8 +8,31 @@ $faker = Faker\Factory::create();
 $db = _db();
 $q = $db->prepare('SELECT user_id FROM users ORDER BY RAND() LIMIT 2');
 $q->execute();
-$ids = $q->fetchAll(PDO::FETCH_COLUMN);
-echo json_encode($ids);
+$ids = $q->fetchAll(PDO::FETCH_COLUMN); // [5,10]
+
+$q = 'INSERT INTO employees VALUES ';
+$values = '';
+$array_length = count($ids);
+for ($i = 0; $i < $array_length; $i++) {
+  $salary = rand(10000, 99999);
+  $index = array_rand($ids);
+  $user_employee_fk = $ids[$index];
+  unset($ids[$index]);
+  $values .= "($user_employee_fk, $salary),";
+}
+$values = rtrim($values, ",");
+$q .= $values;
+
+echo $q;
+$db = _db();
+$sql = $db->prepare($q);
+$sql->execute();
+
+
+
+
+
+
 
 
 exit();
