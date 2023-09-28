2dcba1bb51513b5ec3f40c5b05b8e711724c690b
Santiago Donoso <santiagodonoso@gmail.com>
2023-09-28 11:05:12 +0200

    ...

--- seeders/seed_employees.php
+++ seeders/seed_employees.php
@@ -1,12 +1,33 @@
 <?php
 
+// $ids = [5,6,7,8,9,10]; // Pool
+$ids = ["a","b","c","d","e","f","g"]; // Pool
+$array_length = count($ids);
+// for($i=0; $i<6; $i++){
+for($i=0; $i< $array_length; $i++){
+  $index = array_rand($ids);
+  echo $ids[$index];
+  unset($ids[$index]);
+}
+
+exit();
+
+
+
+
+
+
+
+
+
+
 require_once __DIR__ . '/../_.php';
 require_once __DIR__ . '/Faker/src/autoload.php';
 
 // use the factory to create a Faker\Generator instance
 $faker = Faker\Factory::create();
 
-
+// Seed from ids 5 to 10
 $q = 'INSERT INTO employees VALUES ';
 $values = '';
 for ($i = 0; $i < 10; $i++) {
