01975632126e1300ab9accd0ca3de943a0ae6ace
Santiago Donoso <santiagodonoso@gmail.com>
2023-09-28 13:10:40 +0200

    ...

--- seeders/seed_partners.php
+++ seeders/seed_partners.php
@@ -10,15 +10,14 @@ $q = $db->prepare('SELECT user_id FROM users ORDER BY RAND() LIMIT 2');
 $q->execute();
 $ids = $q->fetchAll(PDO::FETCH_COLUMN); // [5,10]
 
-$q = 'INSERT INTO employees VALUES ';
+$q = 'INSERT INTO partners VALUES ';
 $values = '';
 $array_length = count($ids);
 for ($i = 0; $i < $array_length; $i++) {
-  $salary = rand(10000, 99999);
   $index = array_rand($ids);
-  $user_employee_fk = $ids[$index];
+  $user_partner_fk = $ids[$index];
   unset($ids[$index]);
-  $values .= "($user_employee_fk, $salary),";
+  $values .= "($user_partner_fk, '$faker->latitude,$faker->longitude'),";
 }
 $values = rtrim($values, ",");
 $q .= $values;
