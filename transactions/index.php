767f76d43c1662d9e08311ab30aa65699f48e75c
Santiago Donoso <santiagodonoso@gmail.com>
2023-10-26 09:36:35 +0200

    ...

--- transactions/index.php
+++ transactions/index.php
@@ -4,8 +4,15 @@ try{
   require_once __DIR__.'/_.php';
   $db = _db();
   $db->beginTransaction();
-
+  $transfer = 5;
+  $q = $db->prepare(' UPDATE users 
+                      SET user_balance = user_balance - :transfer
+                      WHERE user_id = 1
+                  ');
+  $q->bindValue(':transfer', $transfer);
+  $q->execute();
   $db->commit();
+  echo 'Transfer OK';
 }catch(Exception $ex){
   echo $ex;
   $db->rollBack();
