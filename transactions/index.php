7bad04f081147d21a5f01b49d0ae627365b3e2b4
Santiago Donoso <santiagodonoso@gmail.com>
2023-10-26 09:52:55 +0200

    ...

--- transactions/index.php
+++ transactions/index.php
@@ -12,7 +12,18 @@ try{
                   ');
   $q->bindValue(':transfer', $transfer);
   $q->execute();
-  
+  // Connect API
+  // Connect API
+  // Connect API
+  // Connect Database
+  // Use a file
+
+
+  if( 'a' != 'b' ){
+    throw new Exception('System under maintainance');
+  }
+
+
   // Add 5 to B
   $q = $db->prepare(' UPDATE users 
                       SET user_balance = user_balance + :transfer
